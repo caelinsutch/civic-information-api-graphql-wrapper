8f1a952d8c9f0525b6e45b4ce7d11bcf2f9ba233
Caelin <caelinsutch@gmail.com>
2020-06-04 11:55:30 -0700

    Forgot all the files

---
+++ src/server.ts
@@ -0,0 +1,17 @@
+import { ApolloServer, gql } from 'apollo-server';
+import resolvers from './resolvers'
+import {CivicAPI} from './datasource';
+const { importSchema } = require('graphql-import')
+const typeDefs = importSchema('src/schema.graphql')
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  dataSources: () => ({
+    civicAPI: new CivicAPI()
+  })
+});
+
+server.listen({ port: process.env.PORT || 4000 }).then(({ url }) => {
+  console.log(`🚀 Server ready at ${url}`);
+});
